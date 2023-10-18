3ee61a914d2d5f71f09650342cef8924ccad286c
mdiasc <milena-.dias@live.com>
2023-10-18 08:48:46 -0300

    teste

---
+++ JavaScript.js/script.js
@@ -0,0 +1,31 @@
+function calculando(num1, num2 operacao){
+    let resposta 
+    if (operacao === "*"){
+        resposta = multiplicar(num1, num2)
+    }else if(operacao === "/"){
+        resposta = divisao(num1,num2)
+    }else if(operacao === "+"){
+        resposta = adicao(num1,num2)
+    }else if(operacao === "-"){
+        resposta = subtracao(num1,num2)
+    }
+
+    
+    document.getElementById("resultado").innerHTML = resposta
+} 
+
+function multiplicar(num1, num2){
+    return num1 * num2
+}
+
+function divisao(num1, num2){
+    return num1 / num2
+}
+
+function adicao(num1, num2){
+    return num1 + num2
+}
+
+function subtracao(num1, num2){
+    return num1 - num2
+}
